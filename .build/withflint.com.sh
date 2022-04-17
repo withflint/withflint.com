54039e08554fd5c809f99a48a112a06ea90bd561
pre63 <pre63@users.noreply.github.com>
2022-04-17 14:02:03 -0700

    update makefile format to sh

--- .build/withflint.com.sh
+++ .build/withflint.com.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-docker build $(pwd) -t $(ECR_URL)/withflint.com:$(GIT_VERSION) --build-arg GIT_VERSION=$(GIT_VERSION) -f $(pwd)/.build/Dockerfile  >&1
-docker push $(ECR_URL)/withflint.com:$(GIT_VERSION)
-echo "- published withflint.com:$(GIT_VERSION)" >> output
+export TARGET="$ECR_URL/withflint.com:$GIT_VERSION"
+
+docker build $(pwd) -t $TARGET --build-arg GIT_VERSION=$GIT_VERSION -f $(pwd)/.build/Dockerfile >&1
+docker push $TARGET >&1
+
+echo "- published withflint.com:$GIT_VERSION" >> output
