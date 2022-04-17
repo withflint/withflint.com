2451a9f4abc716973756e6b044e98fde76d4175b
pre63 <pre63@users.noreply.github.com>
2022-04-17 14:07:49 -0700

    update Docker with latest version for build

--- .build/withflint.com.sh
+++ .build/withflint.com.sh
@@ -4,5 +4,3 @@ export TARGET="$ECR_URL/withflint.com:$GIT_VERSION"
 
 docker build $(pwd) -t $TARGET --build-arg GIT_VERSION=$GIT_VERSION -f $(pwd)/.build/Dockerfile >&1
 docker push $TARGET >&1
-
-echo "- published withflint.com:$GIT_VERSION" >> output
