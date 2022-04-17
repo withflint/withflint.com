7188113e1221d9476cdeaa117256c450126c7155
pre63 <pre63@users.noreply.github.com>
2022-04-17 14:12:06 -0700

    remove output redirects

--- .build/withflint.com.sh
+++ .build/withflint.com.sh
@@ -2,5 +2,5 @@
 
 export TARGET="$ECR_URL/withflint.com:$GIT_VERSION"
 
-docker build $(pwd) -t $TARGET --build-arg GIT_VERSION=$GIT_VERSION -f $(pwd)/.build/Dockerfile >&1
-docker push $TARGET >&1
+docker build $(pwd) -t $TARGET --build-arg GIT_VERSION=$GIT_VERSION -f $(pwd)/.build/Dockerfile
+docker push $TARGET
