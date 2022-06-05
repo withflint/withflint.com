78b9a411c1e11d2b0c3b9c8d14c78c740a41f5fd
pre63 <pre63@users.noreply.github.com>
2022-06-05 12:12:21 -0700

    try outputs

--- .build/withflint.com.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export TARGET="$ECR_URL/withflint.com:$GIT_VERSION"
-
-nix build .#withflint-image
-skopeo copy docker-archive:result docker://$TARGET
-echo "- published withflint.com:$GIT_VERSION"
