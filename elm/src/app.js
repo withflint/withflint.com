1707ec3d547e65cba9e52060bdb1631471ba0359
Azizul Karim <azkarim@users.noreply.github.com>
2022-11-10 23:47:23 +0600

    reset utm url to original page url

--- elm/src/app.js
+++ elm/src/app.js
@@ -23,20 +23,26 @@ const cookie = {
 const url = new URL(window.location.href)
 const path = url.pathname + url.search
 const params = new URLSearchParams(url.search)
-const umamiId = '61e2287e-b2c6-44e9-8446-48339059a08c'
+const utmToken = "utm_source"
+const umamiId = "61e2287e-b2c6-44e9-8446-48339059a08c"
 const umamiScript = document.querySelector(`script[data-website-id="${umamiId}"]`)
 
-if (cookie.get('utm')) {
-  umamiScript.addEventListener('load', () =>
-    umami.trackView(path, 'https://withflint.com', umamiId))
+if (params.has(utmToken)) {
+  const queryParams = params.toString()
+  cookie.set('utm', queryParams)
+  sendToUmami(path)
+  window.history.pushState({}, '', window.location.href.split('?')[0])
+
+} else if (cookie.get('utm')) {
+  const utmParams = cookie.get('utm')
+  const utmUrl = `${path}${utmParams}`
+  sendToUmami(utmUrl)
+
+} else {
+  sendToUmami(path)
 }
 
-// does not assume utm_content is the first param
-if (params.has('utm')) {
-  const utmVal = params.get('utm')
-
-  cookie.set('utm', utmVal)
-  window.history.pushState({}, '', window.location.href.split('?')[0])
+function sendToUmami(path_) {
   umamiScript.addEventListener('load', () =>
-    umami.trackView(path, 'https://withflint.com', umamiId))
+    umami.trackView(path_, 'https://withflint.com', umamiId))
 }
