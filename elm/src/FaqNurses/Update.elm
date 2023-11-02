df31d76169225ec54e0f5a54fdb34742fc98f95a
Ailrk <jimmy123good@gmail.com>
2023-11-02 12:12:58 -0700

    copy2 (#64)

--- elm/src/FaqNurses/Update.elm
+++ elm/src/FaqNurses/Update.elm
@@ -90,22 +90,19 @@ faqs =
       , answer = [ Paragraph "A: The healthcare facilities pay Flint for recruitment services which include finding nurses, licensing, immigration, and relocation. Flint does not charge the nurse as part of the program." ]
       }
     , { question = "**Q: How much support will I get throughout the process?**"
-      , answer = [ Paragraph "A: We will provide you guidance through the whole process sharing next steps along the whole journey. This includes licensing, immigration, and employment arrangements. We also connect you with a network of nurses for support. This network includes nurses who are currently going through the same step of the journey as you and international nurses who are already working in the US." ]
+      , answer = [ Paragraph "A: We will provide you guidance through the process, sharing next steps along the journey. This includes endorsement, immigration, and employment arrangements. We also connect you with a network of nurses for support. This network includes nurses who are currently going through the same step of the journey as you and international nurses who are already working in the US." ]
       }
     , { question = "**Q: Can I bring my family with me?**"
       , answer = [ Paragraph "A: Yes, you will have the opportunity for you to bring spouses and dependents under the age of 21 with you to begin your new life in the US. The costs associated with immigrating and relocating family members are not included in our service. However, we do offer additional financial assistance in the form of a loan to be paid off after you start working. If you want to immigrate with your family make sure to include that in your profile when you are prompted." ]
       }
     , { question = "**Q: Can my spouse work in the US?**"
-      , answer = [ Paragraph "A: Yes, if you have a spouse relocating with you they may be eligible to work in the USA. This is dependent on the type of visa and the terms of your authorization to work in the US. Flints immigration lawyer will advise you on this process." ]
+      , answer = [ Paragraph "A: It depends, if you have a spouse relocating with you they may be eligible to work in the USA. This is dependent on the type of visa and the terms of your authorization to work in the US. Flints immigration lawyer will advise you on this process. Typically, your spouse will need to obtain their own visa to work in the US." ]
       }
     , { question = "**Q: What will my living arrangements be in the US?**"
-      , answer = [ Paragraph "A: Flint will find you a 1-year rental accommodation. This is a typical living arrangement for Americans. We will provide the first month's rent as part of your relocation package." ]
+      , answer = [ Paragraph "A: Flint could help you find rental accommodation for the first couple of months of employment. This is a typical living arrangement for Americans. We will provide the first month's rent as part of your relocation package." ]
       }
     , { question = "**Q: Do I start working right after landing in the US?**"
-      , answer = [ Paragraph "A: You can expect to begin to work at your healthcare facility within one month depending on the requirements of your employer and the government agencies processing your file. This delay generally gives you time to get settled and organized before you start working." ]
-      }
-    , { question = "**Q: How much support will I get after landing in the US?**"
-      , answer = [ Paragraph "A: To help you transition to your new country we also have a relocation advisor to help advise you on matters such as obtaining a driver's license, bank account etc.. Please see our website for more information on US culture and relocation." ]
+      , answer = [ Paragraph "A: You can expect to begin to work at your healthcare facility within two weeks to one month depending on the requirements of your employer and the government agencies processing your file. This delay generally gives you time to get settled and organized before you start working." ]
       }
     , { question = "**Q: How long is the process?**"
       , answer = [ Paragraph "A: The process length varies depending on the person. Typically the process takes between 6 months and two years because we are dependent on the visa processing times." ]
@@ -126,13 +123,13 @@ faqs =
       , answer = [ Paragraph "A: Yes, you need to pass NCELX. We currently don't provide service to nurses without a NCLEX certificate" ]
       }
     , { question = "**Q: If I’m already in the immigration and licensing process can I work with Flint?**"
-      , answer = [ Paragraph "A: We usually can work with you. Reach out to our team to learn more." ]
+      , answer = [ Paragraph "A: Yes, we can usually work with you. Reach out to our team to learn more." ]
       }
     , { question = "**Q: What is the level of English proficiency that is required?**"
-      , answer = [ Paragraph "A: Employers care that you can communicate effectively with staff and patients. They require a professional level of English, which can be demonstrated in the interviews and by passing an English exam. To qualify for Flint’s NCLEX program, you will need to submit proof of a current passing score on the IELTS Academic or TOEFL iBT English exams.  If nurses need additional practice time and would like to improve your English proficiency due to a failed language exam, Flint offers an English as a Second Language course." ]
+      , answer = [ Paragraph "A: Employers care that you can communicate effectively with staff and patients. They require a professional level of English, which can be demonstrated in the interviews and by passing an English exam. To qualify for Flint’s program, you will need to submit proof of a current passing score on the IELTS Academic, TOEFL iBT English exams, etc. If you don’t meet this requirement, you should have a high level of English and be prepared to pass the exam. " ]
       }
     , { question = "**Q: Will I need to take an English test?**"
-      , answer = [ Paragraph "A: Yes, for immigration and licensing you will need to take one of either the IELTS Academic or TOEFL iBT English exam unless your nursing education was in the following countries and your textbooks were in English (United Kingdom, Australia, Canada (except Quebec), New Zealand, Ireland or the United States). If you have already taken an English test recently, we can use those scores. Please submit proof of a passing score to a Flint representative. A passing score on an English exam must be within 2 years of starting the Flint program." ]
+      , answer = [ Paragraph "A: Yes, for immigration and licensing you will need to take one of either the IELTS Academic or TOEFL iBT English exam unless your nursing education was in the following countries and your textbooks were in English (United Kingdom, Australia, Canada (except Quebec), New Zealand, Ireland or the United States). If you have already taken and passed an English test recently, we can use those scores. Please submit proof of a passing score to a Flint representative. A passing score on an English exam must be within 2 years of starting the Flint program." ]
       }
     , { question = "**Q: What is a passing score for the English test?**"
       , answer = [ Paragraph "A: For the IELTS (Academic Format) the overall score must be at least 6.5 and the spoken band 7.0. For the TOEFL iBT you must have an overall score greater than 83 and 26 in the speaking section." ]
@@ -150,10 +147,10 @@ faqs =
       , answer = [ Paragraph "A: Currently we only employ RNs." ]
       }
     , { question = "**Q: What will Flint pay for?**"
-      , answer = [ Paragraph "A: Flint's Registered Nurse Program includes covering expenses associated with: visa filing fees, lawyer fees, Credential evaluation reports, Nursing license application, travel to the US, and initial accommodations in the US. These expenses can total to more than $20,000. Flint doesn’t require the nurses to pay Flint back for any of these expenses after relocation." ]
+      , answer = [ Paragraph "A: Flint's Registered Nurse Program includes covering expenses associated with: visa filing fees, lawyer fees, visascreen, credential evaluation reports, nursing license endorsement application, travel to the US, and initial accommodations in the US. These expenses can total to more than $20,000. Flint doesn’t require the nurses to pay Flint back for any of these expenses after relocation." ]
       }
     , { question = "**Q: What will I need to pay for as a nurse?**"
-      , answer = [ Paragraph "A: There are some services essential to immigration that you may need to pay for yourself in your home country. These services include: background check, medical examination, courier fees, and fees associated with obtaining records from both the institutions where you received your education and from your licensing authorities. The breakdown of these fees will be communicated to you before you need to make a decision. If you need help paying for expenses like these please see our Financial Assistance Program." ]
+      , answer = [ Paragraph "A: There are some services essential to immigration that you may need to pay for yourself in your home country. These services include: background check, medical examination, courier fees, and fees associated with obtaining records from both the institutions where you received your education and from your licensing authorities. The breakdown of these fees will be communicated to you before you need to make a decision. If you need help paying for expenses like these please consult your Nurse Success Advisor." ]
       }
     , { question = "**Q: Will I need to pay Flint money?**"
       , answer = [ Paragraph "A: No, Flint will not ask you for money directly." ]
